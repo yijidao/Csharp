8bf604864e9508afe2a2271415ff279b4fcaf2f1
yijidao <547202221@qq.com>
2018-02-07 09:33:25 +0800

    microsoft sync frame 同步程序

---
+++ Sync/Sync/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Sync
+{
+    partial class FrmSync
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_Source = new System.Windows.Forms.TextBox();
+            this.tbx_Destination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtx_Log = new System.Windows.Forms.RichTextBox();
+            this.btn_Sync = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbx_Filter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源目录";
+            // 
+            // tbx_Source
+            // 
+            this.tbx_Source.Location = new System.Drawing.Point(109, 32);
+            this.tbx_Source.Name = "tbx_Source";
+            this.tbx_Source.Size = new System.Drawing.Size(300, 21);
+            this.tbx_Source.TabIndex = 1;
+            // 
+            // tbx_Destination
+            // 
+            this.tbx_Destination.Location = new System.Drawing.Point(109, 59);
+            this.tbx_Destination.Name = "tbx_Destination";
+            this.tbx_Destination.Size = new System.Drawing.Size(300, 21);
+            this.tbx_Destination.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目标目录";
+            // 
+            // rtx_Log
+            // 
+            this.rtx_Log.Location = new System.Drawing.Point(55, 173);
+            this.rtx_Log.Name = "rtx_Log";
+            this.rtx_Log.Size = new System.Drawing.Size(354, 266);
+            this.rtx_Log.TabIndex = 4;
+            this.rtx_Log.Text = "";
+            // 
+            // btn_Sync
+            // 
+            this.btn_Sync.Location = new System.Drawing.Point(128, 127);
+            this.btn_Sync.Name = "btn_Sync";
+            this.btn_Sync.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sync.TabIndex = 5;
+            this.btn_Sync.Text = "开始同步";
+            this.btn_Sync.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "过滤类型";
+            // 
+            // tbx_Filter
+            // 
+            this.tbx_Filter.Location = new System.Drawing.Point(109, 85);
+            this.tbx_Filter.Name = "tbx_Filter";
+            this.tbx_Filter.Size = new System.Drawing.Size(300, 21);
+            this.tbx_Filter.TabIndex = 7;
+            // 
+            // FrmSync
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 470);
+            this.Controls.Add(this.tbx_Filter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Sync);
+            this.Controls.Add(this.rtx_Log);
+            this.Controls.Add(this.tbx_Destination);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx_Source);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSync";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_Source;
+        private System.Windows.Forms.TextBox tbx_Destination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtx_Log;
+        private System.Windows.Forms.Button btn_Sync;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbx_Filter;
+    }
+}
+
---
+++ Sync/Sync/Form1.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Sync
+{
+    public partial class FrmSync : Form
+    {
+        XmlDocument G_Xml;
+        string G_XmlUrl;
+        public FrmSync()
+        {
+            InitializeComponent();
+            G_XmlUrl = Environment.CurrentDirectory + @"\cf.xml";
+            G_Xml = new XmlDocument();
+            G_Xml.Load(G_XmlUrl);
+            foreach (XmlElement xmlDoc in G_Xml.DocumentElement)
+            {
+                switch (xmlDoc.Name)
+                {
+                    case "SourcePath":
+                        tbx_Source.Text = xmlDoc.InnerText; break;
+                    case "DestinationPath":
+                        tbx_Destination.Text = xmlDoc.InnerText; break;
+                }
+            }
+            btn_Sync.Click += OnClick;
+        }
+
+        public void Sync()
+        {
+            btn_Sync.Enabled = false;
+            string l_SourcePath = tbx_Source.Text;
+            string l_DistinationPath = tbx_Destination.Text;
+            string l_FilterTbx = tbx_Filter.Text;
+            string[] l_FilterTypeArray;
+            l_FilterTypeArray = l_FilterTbx.Split(',');
+            if (string.IsNullOrWhiteSpace(l_SourcePath) || string.IsNullOrWhiteSpace(l_DistinationPath) ||
+                !Directory.Exists(l_SourcePath) || !Directory.Exists(l_DistinationPath))
+            {
+                rtx_Log.AppendText("无效目录");
+                btn_Sync.Enabled = true;
+                return;
+            }
+
+            try
+            {
+                FileSyncOptions l_Options = FileSyncOptions.ExplicitDetectChanges | FileSyncOptions.RecycleConflictLoserFiles |
+                    FileSyncOptions.RecycleDeletedFiles | FileSyncOptions.RecyclePreviousFileOnUpdates;
+                FileSyncScopeFilter l_Filter = new FileSyncScopeFilter();
+
+                foreach (string filterType in l_FilterTypeArray)
+                {
+                    if(!string.IsNullOrWhiteSpace(filterType.Trim()))
+                    {
+                        l_Filter.FileNameExcludes.Add(filterType.Trim());
+                    }
+                }
+                //if(l_FilterTypeArray.Length > 0)
+                //{
+                //    SyncFileSystemReplicasOneWay1(l_SourcePath, l_DistinationPath, l_Filter, l_Options);
+                //}
+                //else
+                //{
+                    SyncFileSystemReplicasOneWay1(l_SourcePath, l_DistinationPath, l_Filter, l_Options);
+                //}
+            }
+            catch (Exception e)
+            {
+               rtx_Log.AppendText("\nException from File Sync Provider:\n" + e.ToString());
+            }
+        }
+
+        public void SyncFileSystemReplicasOneWay1(string p_SourceReplicaRootPath, string p_DestinationReplicaRootPath, FileSyncScopeFilter p_Filter, FileSyncOptions p_Options)
+        {
+            FileSyncProvider l_SourceProvider = null;
+            FileSyncProvider l_DestinationProvider = null;
+
+            try
+            {
+
+                l_SourceProvider = new FileSyncProvider(p_SourceReplicaRootPath, p_Filter, p_Options);
+                l_DestinationProvider = new FileSyncProvider(p_DestinationReplicaRootPath, p_Filter, p_Options);
+
+                l_DestinationProvider.AppliedChange += OnAppliedChange;
+                l_DestinationProvider.SkippedChange += OnSkippedChange;
+                //l_DestinationProvider.DetectedChanges += OnDetectedChange;
+
+                SyncCallbacks l_DestinationCallbacks = l_DestinationProvider.DestinationCallbacks;
+                l_DestinationCallbacks.ItemConflicting += OnItemConflicting;
+                l_DestinationCallbacks.ItemConstraint += OnItemConstraint;
+
+                l_SourceProvider.DetectChanges();
+                l_DestinationProvider.DetectChanges();
+
+                SyncOrchestrator l_Agent = new SyncOrchestrator();
+                l_Agent.LocalProvider = l_DestinationProvider;
+                l_Agent.RemoteProvider = l_SourceProvider;
+                l_Agent.Direction = SyncDirectionOrder.Download;
+
+               rtx_Log.AppendText("\r\n同步文件到：" + l_DestinationProvider.RootDirectoryPath + "");
+                l_Agent.Synchronize();
+            }
+            finally
+            {
+                if (l_SourceProvider != null)
+                    l_SourceProvider.Dispose();
+                if (l_DestinationProvider != null)
+                    l_DestinationProvider.Dispose();
+                btn_Sync.Enabled = true;
+            }
+        }
+
+        public void OnClick(object sender, EventArgs args)
+        {
+            foreach (XmlElement xmlDoc in G_Xml.DocumentElement)
+            {
+                switch (xmlDoc.Name)
+                {
+                    case "SourcePath":
+                        xmlDoc.InnerText = tbx_Source.Text; break;
+                    case "DestinationPath":
+                        xmlDoc.InnerText = tbx_Destination.Text; break;
+                }
+            }
+            G_Xml.Save(G_XmlUrl);
+            Sync();
+        }
+
+        public void OnAppliedChange(Object sender, AppliedChangeEventArgs args)
+        {
+            switch (args.ChangeType)
+            {
+                case ChangeType.Create:
+                   rtx_Log.AppendText("\r\n--创建文件：" + args.NewFilePath);
+                    break;
+                case ChangeType.Delete:
+                   rtx_Log.AppendText("\r\n--删除文件：" + args.OldFilePath);
+                    break;
+                case ChangeType.Update:
+                   rtx_Log.AppendText("\r\n--更新文件：" + args.OldFilePath);
+                    break;
+                case ChangeType.Rename:
+                   rtx_Log.AppendText("\r\n--重命名文件：" + args.OldFilePath + "修改为" + args.NewFilePath);
+                    break;
+            }
+        }
+
+        public void OnSkippedChange(object sender, SkippedChangeEventArgs args)
+        {
+           rtx_Log.AppendText("\r\n--发生错误：" + (!string.IsNullOrEmpty(args.CurrentFilePath) ? args.CurrentFilePath : args.NewFilePath) + "跳过文件" + args.ChangeType.ToString().ToUpper());
+            if (args.Exception != null)
+               rtx_Log.AppendText("[" + args.Exception.Message + "]");
+        }
+
+        public void OnItemConflicting(object sender, ItemConflictingEventArgs args)
+        {
+            args.SetResolutionAction(ConflictResolutionAction.SourceWins);
+           rtx_Log.AppendText("\r\n--Concurrency conflict detected for item" + args.DestinationChange.ItemId.ToString());
+        }
+
+        public void OnItemConstraint(object sender, ItemConstraintEventArgs args)
+        {
+            args.SetResolutionAction(ConstraintConflictResolutionAction.SourceWins);
+           rtx_Log.AppendText("\r\n--Contraint conflict detected for iem" + args.DestinationChange.ItemId.ToString());
+        }
+        public void OnDetectedChange(object sender, DetectedChangesEventArgs args)
+        {
+            rtx_Log.AppendText("\r\n--TotalDirectories" + args.TotalDirectoriesFound);
+            rtx_Log.AppendText("\r\n--TotalFiles" + args.TotalFilesFound);
+            rtx_Log.AppendText("\r\n--TotalFileSize" + args.TotalFileSize);
+        }
+    }
+}
+
