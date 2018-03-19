da08d3412878b0dc3310e871ebc55226f6c9adb1
yijidao <2815451505@qq.com>
2018-03-19 17:16:59 +0800

    使用zxing和GhostScript.net实现PDF转换成PNG

---
+++ ConvertPdfToPng/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtb_Log = new System.Windows.Forms.RichTextBox();
+            this.btn_Todo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Path
+            // 
+            this.tb_Path.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Path.Location = new System.Drawing.Point(54, 26);
+            this.tb_Path.Name = "tb_Path";
+            this.tb_Path.Size = new System.Drawing.Size(317, 21);
+            this.tb_Path.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2, 4, 2, 5);
+            this.label1.Size = new System.Drawing.Size(33, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "路径";
+            // 
+            // rtb_Log
+            // 
+            this.rtb_Log.Location = new System.Drawing.Point(23, 65);
+            this.rtb_Log.Name = "rtb_Log";
+            this.rtb_Log.Size = new System.Drawing.Size(444, 351);
+            this.rtb_Log.TabIndex = 2;
+            this.rtb_Log.Text = "";
+            // 
+            // btn_Todo
+            // 
+            this.btn_Todo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Todo.Location = new System.Drawing.Point(392, 15);
+            this.btn_Todo.Name = "btn_Todo";
+            this.btn_Todo.Size = new System.Drawing.Size(75, 44);
+            this.btn_Todo.TabIndex = 3;
+            this.btn_Todo.Text = "开始报工";
+            this.btn_Todo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 444);
+            this.Controls.Add(this.btn_Todo);
+            this.Controls.Add(this.rtb_Log);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Path);
+            this.Name = "Form1";
+            this.Text = "自动报工";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtb_Log;
+        private System.Windows.Forms.Button btn_Todo;
+    }
+}
+
---
+++ ConvertPdfToPng/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ghostscript.NET.Rasterizer;
+using Ghostscript.NET.Processor;
+using System.IO;
+using System.Drawing.Imaging;
+using ZXing;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string G_DirPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initStyle();
+            btn_Todo.Click += TodoClick;
+        }
+
+        private void TodoClick(object sender, EventArgs e)
+        {
+            G_DirPath = tb_Path.Text;
+            btn_Todo.Enabled = false;
+            tb_Path.Enabled = false;
+            btn_Todo.Text = "报工中";
+            Thread t = new Thread(CreatePicture);
+            t.Start();
+        }
+
+        private void CreatePicture()
+        {
+            while (true)
+            {
+                //string picPath = Environment.CurrentDirectory + @"\picture";
+                while (Directory.GetFiles(G_DirPath).Length > 0)
+                {
+                    string dir = Directory.GetFiles(G_DirPath)[0];
+                    FileStream fs = new FileStream(dir, FileMode.Open);
+                    using (GhostscriptRasterizer r = new GhostscriptRasterizer())
+                    {
+                        r.Open(fs);
+                        for (int i = 0; i < r.PageCount; i++)
+                        {
+                            Image image = r.GetPage(200, 200, i + 1);
+                            Bitmap bitmap = new Bitmap(image);
+                            BarcodeReader reader = new BarcodeReader();
+                            Result result = reader.Decode(bitmap);
+                            if(result != null)
+                            {
+                                print("\r\n--" + result.Text + ",page: " + (i + 1));
+                            }
+                        }
+                    }
+                    fs.Close();
+                    File.Delete(dir);
+                }
+            }
+        }
+
+        private void print(string message)
+        {
+            if (rtb_Log.InvokeRequired)
+            {
+                Action<string> d1 = (x) => { rtb_Log.AppendText(x); };
+                rtb_Log.Invoke(d1, message);
+            }
+        }
+
+        private void initStyle()
+        {
+            label1.ForeColor = Color.FromArgb(255, 255, 255);
+            label1.BackColor = Color.FromArgb(0, 172, 193);
+
+            btn_Todo.ForeColor = Color.FromArgb(255, 255, 255);
+            btn_Todo.BackColor = Color.FromArgb(121, 134, 203);
+            btn_Todo.FlatAppearance.BorderColor = Color.FromArgb(121, 134, 203);
+            btn_Todo.FlatAppearance.MouseOverBackColor = Color.FromArgb(57, 73, 171);
+            btn_Todo.FlatAppearance.MouseDownBackColor = Color.FromArgb(57, 73, 171);
+        }
+    }
+}
